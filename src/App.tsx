a91e7b92e9f921d6bd29ca317e332e0495812d62
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-05-14 13:15:19 +0300

    Feature: SearchBar component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import {type FC} from 'react'
+import type {FC} from "react";
+import SearchBar from "./components/SearchBar.tsx";
+
 
 const App: FC = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div className="app">
+      <h1>TODO LIST</h1>
+      <SearchBar onAdd={(value) => console.log('Добавлено:', value)}/>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import {type ChangeEvent, type FC, type FormEvent, useState} from "react";
+
+type Props = {
+  onAdd: (value: string) => void;
+};
+const SearchBar: FC<Props> = ({onAdd}) => {
+  const [text, setText] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e?: FormEvent) => {
+    e?.preventDefault();
+    const trimmed = text.trim();
+    if (trimmed) {
+      onAdd(trimmed);
+      setText('');
+    }
+  };
+
+  return (
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <input type="text" value={text} onChange={handleChange}/>
+      <button type="submit">Добавить</button>
+    </form>
+  )
+}
+
+export default SearchBar;
